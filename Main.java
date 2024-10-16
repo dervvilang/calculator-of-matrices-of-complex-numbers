1d4e26754d49795ad3f40e647e264b7fa9ae1e31
dervvilang <147318490+dervvilang@users.noreply.github.com>
2024-10-16 19:15:34 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
         complex1.printComplex();
         complex2.printComplex();
 
-
+        //манипулиции с компл.числами
         Complex resAddC = complex1.add(complex2);
         System.out.print("Addition of complex1 and complex2: ");
         resAddC.printComplex();
@@ -25,14 +25,14 @@ public class Main {
         resDivC.printComplex();
         System.out.println();
 
-        //матрицы
-        ComplexMatrix matrix1 = new ComplexMatrix(3, 3);
+        //матрицы создание
+        ComplexMatrix matrix1 = new ComplexMatrix(3, 3); //стандартное задание матрицы
         for (int i = 0; i < matrix1.getRow(); i++) {
             for (int j = 0; j < matrix1.getCol(); j++) {
                 matrix1.setElement(i, j, complex1);
             }
         }
-        Complex[][] arrayForMatr2 = {
+        Complex[][] arrayForMatr2 = { //задание через массив
                 {new Complex(9, 8), new Complex(7, 6), new Complex(3, 2)},
                 {new Complex(5, 4), new Complex(3, 2), new Complex(7, 6)},
                 {new Complex(4, 3), new Complex(2, 1), new Complex(2, 5)},
@@ -42,10 +42,11 @@ public class Main {
         matrix1.printMatrix();
         System.out.println("Second matrix:");
         matrix2.printMatrix();
-        ComplexMatrix matrix3 = ComplexMatrix.copyMatrix(matrix2);
+        ComplexMatrix matrix3 = ComplexMatrix.copyMatrix(matrix2); //копирование матрицы
         System.out.println("Third matrix:");
         matrix3.printMatrix();
 
+        //манипуляции
         ComplexMatrix resAddM = matrix1.add(matrix2);
         System.out.println("Addition of matrix1 and matrix2: ");
         resAddM.printMatrix();
@@ -56,5 +57,13 @@ public class Main {
         System.out.println("Multiplication of matrix1 and matrix2: ");
         resMulM.printMatrix();
 
+        ComplexMatrix matrix4 = new ComplexMatrix(4, 4); //заполнение матрицы рандомными далбами
+        matrix4.Random(100);
+        System.out.println("Random matrix:");
+        matrix4.printMatrix();
+
+        Complex a1 = matrix2.getDeterminant(); //вычисление детерминанта матрицы
+        System.out.println("Determinant of matrix2:" );
+        a1.printComplex();
     }
 }
